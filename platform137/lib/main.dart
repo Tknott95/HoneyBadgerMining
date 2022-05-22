a26f76094209575cf480b4344fc9961c29af8be2
Trevor Knott <tknott95@hotmail.com>
2022-05-22 10:27:50 -0600

    nvidia_smi.sh -f && -t

--- platform137/lib/main.dart
+++ platform137/lib/main.dart
@@ -93,6 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      nvidia_get_fans();
     });
   }
 
