04a70dfb15b4ab9f8cdd7066300135e333b8ddff
Trevor Knott <tknott95@hotmail.com>
2022-05-27 13:49:19 -0600

    THEMES

--- platform137/lib/main.dart
+++ platform137/lib/main.dart
@@ -73,18 +73,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Platform137',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        fontFamily: 'Montserrat',
         primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 50, fontWeight: FontWeight.w800),
+          bodyText1: TextStyle(fontSize: 50, fontWeight: FontWeight.w400),
+        ),
       ),
       home: const MyHomePage(title: 'Platform137'),
     );
