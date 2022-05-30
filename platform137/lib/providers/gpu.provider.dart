e486212df63ddcf06ce2e9244d8fc507b1226a5d
Trevor Knott <tknott95@hotmail.com>
2022-05-29 20:28:00 -0600

    top_section.REFACTOR

--- platform137/lib/providers/gpu.provider.dart
+++ platform137/lib/providers/gpu.provider.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 /* refactor this into a type */
 class GPUProvider extends ChangeNotifier {
   /* default will always be one */
+  /* @TODO fix this possibly with a future return and then afuture builder */
   int _numOfGPUs = 2;
 
   int get numOfGPUs => _numOfGPUs;
