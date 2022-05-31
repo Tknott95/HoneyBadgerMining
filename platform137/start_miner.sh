02bd7e3983337b29cee526f843da0ef006ecf269
Trevor Knott <tknott95@hotmail.com>
2022-05-31 16:10:02 -0600

    start_mining_button.MIDDLE

--- platform137/start_miner.sh
+++ platform137/start_miner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-# POOL=erg.2miners.com:8888	
-# WALLET=9iBNhtz7EsNgzAS43Ad3Hie7TCsbXy2yunAncgHemTMXroXJ93g
+POOL=erg.2miners.com:8888	
+WALLET=9iBNhtz7EsNgzAS43Ad3Hie7TCsbXy2yunAncgHemTMXroXJ93g
 
 cd ../lolminer
 
@@ -15,14 +15,13 @@ ls -la
 
 #   w  - wallet addr
 #   p  - pool
-while getopts 'p:' OPTION; do
+while getopts ':p:' OPTION; do
   case "$OPTION" in
     p)
-      WALLET_ADDR=$(echo $OPTARG | grep -Eo '^[^;]+')
-      echo $WALLET_ADDR
-      POOL_MINING=$(echo $OPTARG | grep -Eo '[^;]*$')
-      echo $POOL_MINING 
-      ./lolMiner --algo AUTOLYKOS2 --pool $POOL_MINING --user $WALLET_ADDR $@ --apiport 1339
+      #WALLET_ADDR=$(echo $OPTARG | grep -Eo '^[^;]+')
+      #echo $WALLET_ADDR
+      # POOL_MINING=$(echo $OPTARG | grep -Eo '[^;]*$')
+      #echo $POOL_MINING 
       # ./lolMiner --algo AUTOLYKOS2 --pool $POOL --user $WALLET $@ --apiport 1339
 
       ;;
@@ -37,3 +36,10 @@ done
 # chmod +x lolMiner
 # echo `./lolMiner --algo AUTOLYKOS2 --pool $POOL --user $WALLET $@ --apiport 1339`
 
+WALLET_ADDR=$(echo $@ | grep -Eo '^[^;]+')
+POOL_MINING=$(echo $@ | grep -Eo '[^;]*$')
+
+echo "MINING POOL:  $POOL_MINING"
+echo "MINING TO:    $WALLET_ADDR"
+
+./lolMiner --algo AUTOLYKOS2 --pool $POOL_MINING --user $WALLET_ADDR $@ --apiport 1339
