64578e725ec1b31552306a517a54d8b3864ca285
Trevor Knott <tknott95@hotmail.com>
2022-05-31 16:13:23 -0600

    start_mining_button.MIDDLE

--- platform137/start_miner.sh
+++ platform137/start_miner.sh
@@ -1,40 +1,12 @@
 #!/bin/bash
 
-
-POOL=erg.2miners.com:8888	
-WALLET=9iBNhtz7EsNgzAS43Ad3Hie7TCsbXy2yunAncgHemTMXroXJ93g
+# EXAMPLE - ./start_miner.sh  "9iBNhtz7EsNgzAS43Ad3Hie7TCsbXy2yunAncgHemTMXroXJ93g;erg.2miners.com:8888"
 
 cd ../lolminer
-
 cd */
 ls -la
 
-# bring in as wallet/pool
-
-
-
-#   w  - wallet addr
-#   p  - pool
-while getopts ':p:' OPTION; do
-  case "$OPTION" in
-    p)
-      #WALLET_ADDR=$(echo $OPTARG | grep -Eo '^[^;]+')
-      #echo $WALLET_ADDR
-      # POOL_MINING=$(echo $OPTARG | grep -Eo '[^;]*$')
-      #echo $POOL_MINING 
-      # ./lolMiner --algo AUTOLYKOS2 --pool $POOL --user $WALLET $@ --apiport 1339
-
-      ;;
-    ?)
-      echo "script usage: $(basename \$0) [-l] [-h] [-a somevalue]" >&2
-      exit 1
-      ;;
-  esac
-done
-
-
-# chmod +x lolMiner
-# echo `./lolMiner --algo AUTOLYKOS2 --pool $POOL --user $WALLET $@ --apiport 1339`
+chmod +x lolMiner
 
 WALLET_ADDR=$(echo $@ | grep -Eo '^[^;]+')
 POOL_MINING=$(echo $@ | grep -Eo '[^;]*$')
