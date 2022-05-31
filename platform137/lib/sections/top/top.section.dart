dc7b60eb4ade1a93ee1c70fba0d5ec58ad9fa801
Trevor Knott <tknott95@hotmail.com>
2022-05-31 01:52:25 -0600

    temp_analytics

--- platform137/lib/sections/top/top.section.dart
+++ platform137/lib/sections/top/top.section.dart
@@ -27,6 +27,10 @@ class TopSection extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children:  <Widget>[
+                            Text(
+                              'STATS FROM OFFICIAL DRIVERS',
+                              style: Theme.of(context).textTheme.bodySmall
+                            ),
                             // Icon(Icons.star, size: 50),
                             if (gpuCount > 1)...[
                              const SizedBox(height: 100, width: 100, 
