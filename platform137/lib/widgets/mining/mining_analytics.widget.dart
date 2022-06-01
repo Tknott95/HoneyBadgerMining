a44d04fe22710f07b5062a6b581da12936f0a005
Trevor Knott <tknott95@hotmail.com>
2022-05-31 18:23:59 -0600

    setting_up_http_stream_for_lominer

--- platform137/lib/widgets/mining/mining_analytics.widget.dart
+++ platform137/lib/widgets/mining/mining_analytics.widget.dart
@@ -89,8 +89,8 @@ class _MiningAnalyWidgetState extends State<MiningAnalyWidget> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text("Algorithm:     ${_minerModel.algorithms![0].algorithm}", style: Theme.of(context).textTheme.bodySmall),
-            Text("GPU_0 Accepted Shares:     ${_minerModel.algorithms![0].totalAccepted}", style: Theme.of(context).textTheme.bodySmall),
-            Text("GPU_0 Hash Rate:     ${_minerModel.algorithms![0].workerPerformance}", style: Theme.of(context).textTheme.bodySmall),
+            Text("Accepted Shares:     ${_minerModel.algorithms![0].totalAccepted}, ${_minerModel.algorithms![1].totalAccepted}", style: Theme.of(context).textTheme.bodySmall),
+            Text("Hash Rate:     ${_minerModel.algorithms![0].workerPerformance}", style: Theme.of(context).textTheme.bodySmall),
 
             Text("STARTUP:      ${_minerModel.session!.startup}", style: Theme.of(context).textTheme.bodySmall),
             Text("last update: ${_minerModel.session!.lastUpdate}" , style: Theme.of(context).textTheme.bodySmall),
