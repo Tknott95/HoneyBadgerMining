79e9d2ab7e59cbea0ae22e296913cd22ba71453b
tknott95 <tknott95@hotmail.com>
2022-09-28 06:33:43 -0600

    globals working

--- platform137/lib/globals/set_globals.dart
+++ platform137/lib/globals/set_globals.dart
@@ -1,5 +1,14 @@
 import 'package:process_run/shell_run.dart';
 
+/* what kind of conventions for my globals? camelCase? snakeCase? etc */
+
+/* Done
+ - Changed to CapitalCase/Public camelCase
+ - nvidia_set_fans
+ - nvidia_set_graphics_clock
+ -
+*/
+
 void NvidiaSetFans(int? _fanIndex, int? _fansVal) {
   var shell = Shell();
   
@@ -8,6 +17,23 @@ void NvidiaSetFans(int? _fanIndex, int? _fansVal) {
   shell.run("""
     #!/bin/bash
     ./nvidia_set.sh -f $fansParam
+    """).then((result){
+      print('Shell script done!');
+    }).catchError((onError) {
+      print('Shell.run error!');
+      print(onError);
+    });
+}
+
+
+/* CLOCKING */
+
+void NvidiaSetGraphicsClock(int _gpuIndex, int _val) {
+  var shell = Shell();
+
+  shell.run("""
+    #!/bin/bash
+    ./nvidia_set.sh -g $_gpuIndex:$_val
     """).then((result){
       print('Shell script done!');
     }).catchError((onError) {
